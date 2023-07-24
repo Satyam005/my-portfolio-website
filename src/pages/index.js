361ce0dad080636f38e57293bf992315a41a9e95
Satyam Negi <sn.satyam2014@gmail.com>
2023-07-24 16:54:53 +0530

    Updated Resume link

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/satyam_resume.pdf"
+                  href="https://drive.google.com/file/d/1xsmSCPhZxLDKmQLvnpzzCEg_e0dUU5IT/view?usp=sharing"
                   target="_blank"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg
                    font-semibold hover:bg-light hover:text-dark 
